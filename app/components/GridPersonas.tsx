559906b1a350fc1ab5421ad14f21dc8706d785e0
Elena-Alvarez-Campos <elenaalvarezcampos8@gmail.com>
2026-05-29 12:02:40 +0200

    retoques

--- app/components/GridPersonas.tsx
+++ app/components/GridPersonas.tsx
@@ -4,7 +4,9 @@ export default function GridPersonas(){
     let edad=30
     let profesion="Médica"
     let nombre2="Lucía"
-    return (<div>
+    return (<div className="grid grid-cols-2 gap-0.25 justify-evenly w-100">
+        <TarjetaPersonas nombre={nombre} edad={edad} profesion={profesion}></TarjetaPersonas>
+        <TarjetaPersonas nombre={nombre2} edad={edad} profesion={profesion}></TarjetaPersonas>
         <TarjetaPersonas nombre={nombre} edad={edad} profesion={profesion}></TarjetaPersonas>
         <TarjetaPersonas nombre={nombre2} edad={edad} profesion={profesion}></TarjetaPersonas>
         </div>)
--- app/components/TarjetaPersonas.tsx
+++ app/components/TarjetaPersonas.tsx
@@ -5,7 +5,7 @@ type GridPersonas={
         profesion:string
     }
 export default function({nombre,edad,profesion}:(GridPersonas)){
-    return(<div className="bg-blue-300 m-2">
+    return(<div className="grid grid-cols-1 gap-2 text-center w-30 bg-blue-300 m-2">
         <p>{nombre}</p>
         <p>{edad}</p>
         <p>{profesion}</p>
