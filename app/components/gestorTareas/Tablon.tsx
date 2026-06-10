a08d6746cb68c9339f2705a6e93da45336ab685c
Elena-Alvarez-Campos <elenaalvarezcampos8@gmail.com>
2026-06-10 19:53:05 +0200

    sin retocar prisma studio

--- app/components/gestorTareas/Tablon.tsx
+++ app/components/gestorTareas/Tablon.tsx
@@ -1,6 +1,9 @@
-"use client"
+"use server"
+import { revalidatePath } from "next/cache"
+import { insertarTarea } from "@/lib/tareas-prisma"
+import { obtenerTareas } from "@/lib/tareas-prisma"
+
 import { Button } from "@/components/ui/button"
-import {useState} from "react";
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Switch } from "@/components/ui/switch"
@@ -21,6 +24,7 @@ import {
   AlertDescription,
   AlertTitle,
 } from "@/components/ui/alert"
+
 type Tarea={
     id:number,
     titulo: string,
@@ -28,54 +32,45 @@ type Tarea={
 }
 
 
-export default function Tablon(){
-    const [titulo, setTitulo] = useState("")//título que empieza vacío
-    const [descripcion, setDescripcion] = useState("")//descripción qiue empieza vacío
-    const [tarea, setTareas] = useState<Tarea[]>([])//array tareas creadas que empieza vacío
-    const [advertencia,setAdvert]=useState(false)
-    const [vacio,setvacio]=useState(true)
-    function anadirTarea() {
-        if (titulo.trim() === "" || descripcion.trim() === "") {
-            setAdvert(true)
+export default async function Tablon(){
+    const tareas = await obtenerTareas()
+    async function crearTarea(formData: FormData) {
+        const titulo = formData.get("titulo")
+        const descripcion = formData.get("descripcion")
+        if (typeof titulo !== "string" || typeof descripcion !== "string") {
             return
         }
-        setvacio(false)
-        setAdvert(false)
-        const nuevaTarea: Tarea={
-            id: Date.now(),
-            titulo: titulo.trim(),
-            descripcion: descripcion.trim(),
+        if (titulo.trim() === "" || descripcion.trim() === "") {
+            return
         }
-        //let nuevaTarea=<Tareas id={Date.now()} titulo={titulo.trim()} descripcion={descripcion.trim()}></Tareas>
-        
-        setTareas([...tarea,nuevaTarea]);
-        setTitulo("")
-        setDescripcion("")
+        await insertarTarea(titulo.trim(), descripcion.trim())
+        revalidatePath("/gestor-tareas")
     }
+        
     //#00786f
     return(<div className="pl-3 flex flex-row min-h-screen bg-teal-100">
         <div className=" min-w-1/2">
             <h1 className="p-1 ml-10 pt-4 text-2xl font-bold text-teal-600">Gestor de tareas básico</h1>
-            <div>
-                <Input className="max-w-70 m-1 ml-10 bg-white" value={titulo} placeholder="Escribe un título" onChange={e => setTitulo(e.target.value)}></Input>
-                <Textarea className="max-w-90 m-1 ml-10 bg-gray-200" value={descripcion} placeholder="Escribe una descripción" onChange={e => setDescripcion(e.target.value)}></Textarea>
+            <form action={crearTarea}>
+                <div>
+                    <Input className="max-w-70 m-1 ml-10 bg-white" name="titulo" placeholder="Escribe un título" ></Input>
+                    <Textarea className="max-w-90 m-1 ml-10 bg-gray-200" name="descripcion" placeholder="Escribe una descripción"></Textarea>
+                    <Button  className="max-w-30 m-1 ml-10" >Nueva Tarea</Button>
                 
-                <Button  className="max-w-30 m-1 ml-10" onClick={anadirTarea}>Nueva Tarea</Button>
-                <p className="p-1 ml-10 pb-3 text-s font-bold text-teal-600">Total de tareas: {tarea.length}</p>
-                <Alert variant="destructive" className={`max-w-md ml-10 border-0 ${advertencia ? 'block' : 'hidden'}`}>
-                    <AlertTitle>No puedes dejar ningún campo vacío</AlertTitle>
-                </Alert>
-                
-            </div>
+                </div>
+            </form>
+            
         </div>
 
         <div className="bg-cyan-800 min-w-1/2 flex justify-center">
+        {tareas.length === 0 ? (    
             <div>
-            <Alert variant="destructive" className={`max-w-md border-0 bg-transparent ${vacio ? 'block' : 'hidden'}`}>
+            <Alert variant="destructive" className={`max-w-md border-0 bg-transparent `}>
                 <AlertTitle className="text-white bg-transparent text-center text-lg">Todavía no tienes tareas</AlertTitle>
             </Alert>
             </div>
-            <div >{tarea.map((tarea) => (
+        ) : (
+            <div >{tareas.map((tarea) => (
                 <div key={tarea.id}>
                 <Card className="min-w-50  m-3 p-4 pl-4 h-auto">
                     <div>
@@ -88,6 +83,7 @@ export default function Tablon(){
                 </Card>
                 </div>
             ))}</div>
+        )}
         </div>
     </div>)
 }
