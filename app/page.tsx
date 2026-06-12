860c590602c96c65c528fb05771549a712746754
Elena-Alvarez-Campos <elenaalvarezcampos8@gmail.com>
2026-06-12 16:16:32 +0200

    prueba sin pulir

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
-//import Hero from "./components/otrosProyectos/ternario";
-import GridPersonas from "./components/otrosProyectos/GridPersonas"
-import Ternario from "./components/otrosProyectos/ternario";
+
 import Tablon from "./components/gestorTareas/Tablon";
 //import tarea from "./components/tarea"
 export default function Home() {
